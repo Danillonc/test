502b06f171f37e2230df3d5712e1ea186a801116
danillonc@live.com <danillonc@live.com>
2016-12-06 13:54:56 -0200

    implementação de teste unitarios

--- fiveware-test-web/src/test/java/br/fireware/application/test/FirewareIntegrationTest.java
+++ fiveware-test-web/src/test/java/br/fireware/application/test/FirewareIntegrationTest.java
@@ -1,5 +1,40 @@
 package br.fireware.application.test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import br.fiveware.application.Application;
+import junit.framework.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class FirewareIntegrationTest {
+
+	@Value("${local.server.port}") // 6
+	int port;
+
+	// @Autowired
+	// private TestRestTemplate restTemplate;
+
+	/**
+	 * Método responsável por testar a inicialização da aplicação.
+	 * @throws Exception Exceção ao inicializar a aplicação.
+	 */
+	@Test
+	public void testInicioFireware() throws Exception {
+		ResponseEntity<String> entity = new TestRestTemplate().getForEntity("http://localhost:" + this.port + "/",
+				String.class);
+		Assert.assertEquals(HttpStatus.OK, entity.getStatusCode());
+	}
 
 }
